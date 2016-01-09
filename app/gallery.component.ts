79d71e07aae47d4a7a769eae4361f4cf92b2253e
Mika Turunen <mika.turunen@ymail.com>
2016-01-09 09:50:08 +0200

    Start fleshing the component gallery together slowly.

---
+++ app/gallery.component.ts
@@ -0,0 +1,13 @@
+///
+
+"use strict";
+
+import { Component } from "angular2/core";
+
+@Component({
+    selector: "gallery-component",
+    templateUrl: "release/templates/gallery-component.html"
+})
+export class GalleryComponent {
+
+}
